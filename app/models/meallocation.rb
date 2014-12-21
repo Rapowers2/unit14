98282805994702207047e435ae0ab4cd3d77bf0f
Rapowers2 <Rapowers2@wisc.edu>
2014-12-20 22:28:53 -0600

    Update meallocation.rb

--- app/models/meallocation.rb
+++ app/models/meallocation.rb
@@ -1,2 +1,6 @@
 class Meallocation < ActiveRecord::Base
+  belongs_to :meal # this means there is a foreign key column on the meallocation table pointing to meal table
+  validates :meal, presence:  true
+  belongs_to :location # this means there is a foreign key column on the meallocation table pointing to the location table
+  validates :location, presence: true
 end

fedcab5b2e176e49bfc0da894569f6d01497d0f9
Rapowers2 <Rapowers2@wisc.edu>
2014-12-20 22:31:26 -0600

    Update drink.rb

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,2 +1,5 @@
 class Drink < ActiveRecord::Base
+  has_one :meal # this means there is a foreign key column on the meal table pointing to the drink table
+  validates :meal, presence: true
+  validates :meal, uniqueness: true
 end

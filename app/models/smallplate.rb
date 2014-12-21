4838445d7f2f8f03777c172b94e8cf4991c8c5e8
Rapowers2 <Rapowers2@wisc.edu>
2014-12-20 22:27:59 -0600

    Update smallplate.rb

--- app/models/smallplate.rb
+++ app/models/smallplate.rb
@@ -1,2 +1,5 @@
-class Small < ActiveRecord::Base
+class Smallplate < ActiveRecord::Base
+  has_one :meal # this means there is a foreign key column on the meal table pointing to the small table
+  validates :meal, presence: true
+  validates :meal, uniqueness: true
 end

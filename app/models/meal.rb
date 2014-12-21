d557e4ba26a7d921777e5197ac2a61a205939a09
Rapowers2 <Rapowers2@wisc.edu>
2014-12-20 22:29:43 -0600

    Update meal.rb

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,12 @@
 class Meal < ActiveRecord::Base
+  belongs_to :small # this means there is a foreign key column on the meal table pointing to smallplate table
+  validates :small, presence: true
+  belongs_to :dessert # this means there is a foreign key column on the meal table pointing to dessert table
+  validates :dessert, presence: true 
+  belongs_to :drink # this means there is a foreign key column on the meal table pointing to drink table
+  validates :drink, presence: true
+  has_many :location, through => :meallocation #through the meallocation bridge
+  validates: location, presence: true
+  has_many :meallocation #meallocation foreign key points to meallocation table
+  validates :meallocation, presence: true
 end
